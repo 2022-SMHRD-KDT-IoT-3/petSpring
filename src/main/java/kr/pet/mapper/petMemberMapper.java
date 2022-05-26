a880ad2367341880b4374a17c13528bb511ba913
gogo <goeun0311@gmail.com>
2022-05-26 15:26:44 +0900

    멤버 회원가입 로긴 연결

--- src/main/java/kr/pet/mapper/petMemberMapper.java
+++ src/main/java/kr/pet/mapper/petMemberMapper.java
@@ -1,8 +1,27 @@
 package kr.pet.mapper;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
+
+import kr.pet.mapper.petMemberVO;
+
+
+
+
 
 @Mapper
 public interface petMemberMapper {
+
+	petMemberVO loginSelectOne(petMemberVO vo);
+	
+	public void joinInsert(petMemberVO vo);
+	
+	public List<petMemberVO> memberList();
+
+	public petMemberVO idCheck(String id);
+	
+	public void updateService(petMemberVO vo);
+
 
 }
--- src/main/java/kr/pet/web/petMemberController.java
+++ src/main/java/kr/pet/web/petMemberController.java
@@ -1,10 +1,16 @@
 package kr.pet.web;
 
+import java.util.List;
+
 import javax.inject.Inject;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import kr.pet.mapper.petMemberMapper;
+import kr.pet.mapper.petMemberVO;
 
 @Controller
 public class petMemberController {
@@ -12,6 +18,53 @@ public class petMemberController {
 	@Inject
 	private petMemberMapper mapper;
 	
+	
+	
+	@RequestMapping("/joinInsert.do")
+	public void joinInsert(petMemberVO vo) {
+		System.out.println(vo.toString());
+		mapper.joinInsert(vo);
+		System.out.println("회원가입기능요청");
+		
+	}
+	
+	
+	@RequestMapping("/loginSelectOne.do") 
+	public String loginSelectOne(HttpSession session, petMemberVO vo) {
+		System.out.println("로그인기능실행");
+		petMemberVO info =  mapper.loginSelectOne(vo);
+		System.out.println(info);
+		if(info!=null) {
+			session.setAttribute("info", info);
+		}
+		return "redirect:/boardList.do";
+	}
+	
+	
+	
+	@RequestMapping("/updateService.do")
+	public void updateService(HttpSession session, petMemberVO vo) {
+		System.out.println("회원정보실행");
+		mapper.updateService(vo);
+		session.setAttribute("info", vo);
+	
+	}
+	
+	@RequestMapping("/memberList.do")
+	public @ResponseBody List<petMemberVO> memberList() {
+		System.out.println("멤버목록 비동기 통신");
+		List<petMemberVO> list = mapper.memberList();
+		return list;
+	}
+	
+	@RequestMapping("/andLogin.do")
+	public @ResponseBody petMemberVO andLogin(petMemberVO vo){
+		petMemberVO info = mapper.loginSelectOne(vo);
+		System.out.println(info);
+		
+		return info;
+		
+	}
 	
 	
 	
