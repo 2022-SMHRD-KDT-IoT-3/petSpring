9da7c989be171ff602f90d39876ae7dc65541a2a
gogo <goeun0311@gmail.com>
2022-05-28 16:16:52 +0900

    로그인 컨트롤러족ㅁㄱ변함

--- target/classes/kr/pet/mapper/petSql.sql
+++ target/classes/kr/pet/mapper/petSql.sql
@@ -5,3 +5,6 @@ select * from tbl_cage;
 select * from tbl_dog;
 select * from tbl_cageinfo;
 select * from tbl_cmt;
+
+insert into tbl_member values("gogo1","1234","고고",
+"고고고고고","01001-0010","광주 북구","2022-05-28","유저");
