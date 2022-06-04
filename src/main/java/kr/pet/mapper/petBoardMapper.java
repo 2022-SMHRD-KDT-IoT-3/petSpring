9ee3da83ef1fc4083dd6854bd5d3c851806b93d1
gogo <goeun0311@gmail.com>
2022-06-04 17:27:52 +0900

    보드컨트롤러 추가됨 매퍼, xml 댓글 조회, 등록, 게시글 조회 추가

--- src/main/java/kr/pet/mapper/petBoardMapper.java
+++ src/main/java/kr/pet/mapper/petBoardMapper.java
@@ -10,6 +10,20 @@ public interface petBoardMapper {
 
 	public List<petBoardVO> boardList();
 
-	public petBoardVO boardInsert(petBoardVO vo);
+	public void boardInsert(petBoardVO vo);
+
+	public void count(int idx);
+
+	public petBoardVO boardContents(int idx);
+
+	public void boardUpdate(petBoardVO vo);
+
+	public void boardDelete(int idx);
+
+	
+	// 아래부터 댓글 기능
+	public List<CmtVO> cmtList(String board_seq);
+
+	public void cmtInsert(CmtVO vo);
 
 }
